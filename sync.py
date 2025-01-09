bb02028bfc39d7ea982f34f89d5d328f23b24eaa
LDH <hyuki0003@kw.ac.kr>
2025-01-09 13:18:28 +0900

    cubic interpolation

---
+++ sync.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+from scipy.interpolate import interp1d
+from scipy.interpolate import CubicSpline
+
+def interpolate_1Dsignals(df, target_rate):
+ 
+    time_column = df.columns[0]
+    signal_columns = df.columns[1:]
+
+    original_time = df[time_column].values
+
+    target_time = np.arange(original_time[0], original_time[-1], 1 / target_rate)
+
+    interpolated_data = {'timestamp': target_time}
+
+    for column in signal_columns:
+        signal = df[column].values
+        interpolator = interp1d(original_time, signal, kind='cubic', fill_value="extrapolate")
+        interpolated_data[column] = interpolator(target_time)
+
+    interpolated_df = pd.DataFrame(interpolated_data)
+
+    return interpolated_df
+
+def interpolate_video(df, video, target_rate):
+
+    time_column = df.columns[0]
